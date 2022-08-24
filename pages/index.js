d29d36156776cc94dce7fb28545f7ee791b051c0
Natthanan03 <kwangaways1319@gmail.com>
2022-08-24 09:39:35 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
@@ -51,7 +52,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -64,6 +64,8 @@ export default function Home() {
           </span>
         </a>
       </footer>
+      
     </div>
+    
   )
 }
